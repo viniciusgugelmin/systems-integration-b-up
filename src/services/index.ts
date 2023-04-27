ec7214d5a730780259d775af999b05c8bbf82a5e
Aramischangchain <aramischangchain@gmail.com>
2023-04-27 20:59:22 -0300

    add rota

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,11 +2,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const folhasProcessadas = []
+
 class RootService {
-  public async get(): Promise<string> {
+  public async get() {
     const apiA = process.env.API_A;
 
-    return apiA;
+    return {
+      folhasProcessadas,
+      totalSalarioLiquido: folhasProcessadas.reduce((total,folha) => total + folha.liquido,0)
+    };
   }
 }
 
